ca2fee735f07cb8d174c1b53b28d979b8825414c
NelsonFM23 <133304859+NelsonFM23@users.noreply.github.com>
2023-07-20 02:00:18 +0200

    act 4

--- src/pages/LandingPage2.jsx
+++ src/pages/LandingPage2.jsx
@@ -1,6 +1,6 @@
 import {ContextMovieCard} from "../components/ContextMovieCard"
 export function LandingPage(){
     return(<div>
-        <ContextMovieCard/>hola
+        <ContextMovieCard/>
     </div>)
 }
--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage } from "../pages/LandingPage";
+import { LandingPage } from "../pages/LandingPage2";
 import { MovieDetails } from "../pages/MovieDetails";
 export function MyRoutes() {
   return (
